94760a4e6076c74ef4bcdae10cddee343189e352
Alina Sydoruk <alinasydoruk@ongoing.ch>
2024-12-17 14:40:40 +0100

    fix fixed problem with Date

--- src/Service/ImportService.php
+++ src/Service/ImportService.php
@@ -7,7 +7,6 @@ use App\Entity\AbsenceType;
 use App\Repository\AbsenceRepository;
 use App\Repository\EmployeeRepository;
 use DateTime;
-use Gedmo\Translator\TranslationInterface;
 use Pagerfanta\Exception\LogicException;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
@@ -35,78 +34,69 @@ readonly class ImportService
         foreach ($worksheet->getRowIterator(2) as $row) {
             $cellIterator = $row->getCellIterator();
             $cellIterator->setIterateOnlyExistingCells(true);
-
             $cells = [];
-
-            foreach ($cellIterator as $cell) {
-                $cells[] = $cell->getValue();
-            }
-
-            //email
-            $employee = $this->employeeRepository->findEmployeeByEmail($cells[0]);
-
-            if (!$employee) {
-                throw new LogicException("Unable to find employee with email : $cells[0]");
-            }
-
-            //AbsenceType
-             // for future
-            // $type =  $this->translator->trans('absence.type_'. strtolower( str_replace( ' ', '_' ,$cells[1])));
-
-
-
-
-            $absenceType = AbsenceType::tryFrom(trim($cells[1]));
-
-            if (!$absenceType) {
-                throw new \LogicException("Missing or incorrect Absence Type : $cells[1] , only ".  implode(', ', array_map(fn($case) => $case->value, AbsenceType::cases())) ."are allowed ");
-            }
-
-            $comment = $cells[2];
-            $absence = new Absence($employee);
-
-            //StartDate
-            $startDate = null;
-
-            $date = ($cells[3]);
-            if (is_int($date) && Date::isDateTimeFormat($date)) {
-                $startDate = Date::excelToDateTimeObject($date);
-                $absence->setStartDate($startDate);
-            } else if($startDate = DateTime::createFromFormat('d.m.Y', $date)){
-                    $absence->setStartDate($startDate);
-            } else throw new \LogicException("Incorrect date type: $date, try dd.mm.yyyy.  " );
-
-
-            //EndDate
-            $endDate = null;
-
-            $dateString = trim($cells[4]);
-            if (is_int($date) && Date::isDateTimeFormat($dateString)) {
-                $endDate = Date::excelToDateTimeObject($dateString);
-                $absence->setEndDate($endDate);
-            } else
-                try {
-                    $endDate = DateTime::createFromFormat('d.m.Y', $dateString);
-                    $absence->setEndDate($endDate);
-                } catch (\Exception $e) {
-                    throw new \LogicException("Incorrect date type: $dateString, try dd.mm.yyyy. Error: " . $e->getMessage());
+            if(!$row->isEmpty()) {
+                foreach ($cellIterator as $cell) {
+                    $cells[] = $cell->getValue();
+                }
+                if (sizeof($cells) < 5) {
+                    throw new \LogicException("where are empty cells in row " . $row->getRowIndex());
                 }
 
+                    //email
+                    $employee = $this->employeeRepository->findEmployeeByEmail($cells[0]);
 
-            $absence->setAbsenceType($absenceType);
-            $absence->setComment($comment);
+                    if (!$employee) {
+                        throw new LogicException("Unable to find employee with email : $cells[0]");
+                    }
+
+                    $absenceType = AbsenceType::tryFrom(trim($cells[1]));
+
+                    if (!$absenceType) {
+                        throw new \LogicException("Missing or incorrect Absence Type : $cells[1] , only " . implode(', ', array_map(fn($case) => $case->value, AbsenceType::cases())) . "are allowed ");
+                    }
+
+                    $comment = $cells[2];
+                    $absence = new Absence($employee);
+
+                    //StartDate
+                    $startDate = null;
+
+                    $date = trim($cells[3]);
+                    if (is_numeric($date)) {
+                        $startDate = Date::excelToDateTimeObject(intval($date));
+                        $absence->setStartDate($startDate);
+                    } else if ($startDate = DateTime::createFromFormat('d.m.Y', $date)) {
+                        $absence->setStartDate($startDate);
+                    } else throw new \LogicException("Incorrect date type: $date, try dd.mm.yyyy.  ");
 
 
-            $errors = $this->validator->validate($absence);
-            if (count($errors) > 0) {
-                $errorsString = '';
-                foreach ($errors as $violation) {
-                    $errorsString = $violation->getPropertyPath() . ': ' . $violation->getMessage() . "\n";
+                    //EndDate
+                    $endDate = null;
+                    $date = trim($cells[4]);
+                    if (is_numeric($date)) {
+                        $endDate = Date::excelToDateTimeObject(intval($date));
+                        $absence->setEndDate($endDate);
+                    } else if ($endDate = DateTime::createFromFormat('d.m.Y', $date)) {
+                        $absence->setEndDate($endDate);
+                    } else throw new \LogicException("Incorrect date type: $date, try dd.mm.yyyy.  ");
+
+
+                    $absence->setAbsenceType($absenceType);
+                    $absence->setComment($comment);
+
+
+                    $errors = $this->validator->validate($absence);
+                    if (count($errors) > 0) {
+                        $errorsString = '';
+                        foreach ($errors as $violation) {
+                            $errorsString = $violation->getPropertyPath() . ': ' . $violation->getMessage() . "\n";
+                        }
+                        throw new \LogicException(' Absences are not valid for this employee ' . $absence->getEmployee()->getEmail() . ' ' . $errorsString);
+                    }
+                    $absences[] = $absence;
                 }
-                throw new \LogicException($errorsString);
             }
-            $absences[] = $absence;
-        }
 
         foreach ($absences as $absence ){
             $this->absenceRepository->save($absence);
