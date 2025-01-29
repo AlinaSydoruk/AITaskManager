957a11c05dda40f0225620b3ae4bda675e11bd60
Alina Sydoruk <alinasydoruk@ongoing.ch>
2025-01-29 18:03:48 +0100

    feat PRAK-3 enabled SoftDeleteable behavior for Employee entity

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -7,11 +7,16 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\SoftDeleteable;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Validator\Constraints  as  Assert;
 
 #[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+#[SoftDeleteable(fieldName: 'deletedAt')]
 class Employee
 {
+    use TimestampableEntity,  SoftDeleteableEntity;
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'AUTO')]
     #[ORM\Column]
