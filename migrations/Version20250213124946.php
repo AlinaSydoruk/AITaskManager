989c97a65fbd6d5152cb89fb1a3235667c4e972c
Basil Baumgartner <basil.baumgartner@ongoing.ch>
2025-02-14 11:11:46 +0100

    chore: updated migrations

--- migrations/Version20250213124946.php
+++ migrations/Version20250213124946.php
@@ -14,13 +14,14 @@ final class Version20250213124946 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'ALTER TABLE absence ADD created_at, updated_at, deleted_at';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE absence ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, ADD deleted_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE absence ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL, ADD deleted_at DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE absence SET created_at = NOW(), updated_at = NOW()');
+        $this->addSql('ALTER TABLE absence MODIFY created_at DATETIME NOT NULL, MODIFY updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
